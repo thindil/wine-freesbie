649b6574cf8f60aa59a071258cc48f7677cbb3f7
Bartek Jasicki <3816565+thindil@users.noreply.github.com>
2024-07-11 18:41:52 +0200

    Update to FreeBSD 14.1

--- freesbie.sh
+++ freesbie.sh
@@ -37,7 +37,7 @@ export FREESBIE_DIR="$HOME/freesbie"
 # of FreeBSD. For example, for 13.2 it will be 13.
 abiVersion=14
 # The FreBSD version for packages.
-freebsdVersion=14.0
+freebsdVersion=14.1
 
 # If the user not entered a command, show the list of available commands
 if [ $# -eq 0 ]; then
